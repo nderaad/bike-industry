659d77a33c45ec9c059b621c5ee9fde292ab4e54
nderaad <nickderaad@gmail.com>
2016-09-05 01:18:41 +0000

    Initial Commit

---
+++ bikes/.~c9_invoke_0Og31.py
@@ -0,0 +1,18 @@
+
+class bike(object):
+    def __init__(self,model,weight,cost):
+        self.model = model
+        self.weight = weight
+        self.cost = cost
+    
+class store(object):
+    def __init__(self,name,inventory,margin,profit):
+        self.name = name
+        self.inventory = inventory
+        self.margin = margin
+        self.profit -
+
+class 
+
+class consumer(object):
+    
---
+++ bikes/bikes.py
@@ -0,0 +1,20 @@
+
+class bike(object):
+    def __init__(self,model,weight,cost):
+        self.model = model
+        self.weight = weight
+        self.cost = cost
+    
+class store(object):
+    def __init__(self,name,inventory,margin,profit):
+        self.name = name
+        self.inventory = inventory
+        self.margin = margin
+        self.profit = profit
+        print('You created {}, a bike shop with {} bikes, {} margin, and {} profits so far'.format(self.name, self.inventory, self.margin, self.profit))
+class customers(object)
+    def __init__(self,name,fund,bought):
+        self.name = name
+        self.fund = fund
+        self.bought = bought
+        
